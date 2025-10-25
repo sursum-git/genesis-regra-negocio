68095e275fc4e863f289e7b7cdf9053d861b25ba
Sursum Corda Soluções <110403188+sursum-git@users.noreply.github.com>
2025-10-25 12:08:00 -0300

    Change process method return type to void

--- RegraNegocio/IRegraNegocio.php
+++ RegraNegocio/IRegraNegocio.php
@@ -4,7 +4,7 @@ namespace Genesis\RegraNegocio;
 interface IRegraNegocio {
     public function setParams(array $params): void;
     public function setLogger(\Psr\Log\LoggerInterface $logger): void;
-    public function process(): mixed;
+    public function process(): void;
     public function getResults(): mixed;
     public function hasError(): bool;
     public function getErrors(): array;
